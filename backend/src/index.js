253a6baa01ad36469b2b8ef350a0994d5c97ff38
wenderson-me <wendrson22@gmail.com>
2020-03-28 22:02:35 -0300

    Validações no BackEnd
    usando a biblioteca celebrate, o campo whatsapp ta bugadão, depois olho essa bagaça

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 const cors = require ('cors');
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json()); //requisição json
 app.use(routes);
+app.use(errors());
 
 
 /**
